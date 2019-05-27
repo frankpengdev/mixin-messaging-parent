9a7f06c8226797dce2f138bea4a8eeff5219bfa8
luojinjun <luojinjun@yodoo.net.cn>
2019-05-27 13:32:13 +0800

    修改继承yodoo-megalodon-parent父工程

--- md-messaging-core/src/main/java/com/feikongbao/messaging/core/config/MessagingCoreConfig.java
+++ md-messaging-core/src/main/java/com/feikongbao/messaging/core/config/MessagingCoreConfig.java
@@ -16,7 +16,6 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
