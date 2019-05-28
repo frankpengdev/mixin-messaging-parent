691fe383d3cc62ade6a9bd1b9557fccdf2d89034
wangzili <wangzili@yodoo.net.cn>
2019-05-28 20:30:28 +0800

    配置APOLLO,删除MybatisGenerator http://crucible.feikongbao.cn/cru/CR-94

--- md-messaging-wechat/src/main/java/com/feikongbao/message/wechat/config/MessageWeChatConfiguration.java
+++ md-messaging-wechat/src/main/java/com/feikongbao/message/wechat/config/MessageWeChatConfiguration.java
@@ -1,6 +1,7 @@
 package com.feikongbao.message.wechat.config;
 
 import com.feikongbao.messaging.core.config.MessagingCoreConfig;
+import com.yodoo.megalodon.datasource.annotation.EnableCompanyDataSource;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.ExecutorType;
@@ -12,16 +13,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.*;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
@@ -41,10 +37,10 @@ import java.util.Base64;
         sqlSessionTemplateRef = MessageWeChatConfiguration.WECHAT_SQL_SESSION_TEMPLATE_BEAN_NAME)
 @EnableTransactionManagement
 @Import({MessageWeChatRestTemplateConfiguration.class,
-        MessageWeChatCacheConfiguration.class,
         MessagingCoreConfig.class,
         MessageWeChatMvcWebAppConfigurer.class})
 @Configuration
+@EnableCompanyDataSource
 public class MessageWeChatConfiguration {
 
     private Logger logger = LoggerFactory.getLogger(MessageWeChatConfiguration.class);
@@ -53,60 +49,26 @@ public class MessageWeChatConfiguration {
     public static final String WECHAT_SQL_SESSION_TEMPLATE_BEAN_NAME = "messageWeChatSqlSessionTemplate";
     private static final String WECHAT_DATA_SOURCE_TRANSACTION_MANAGER_BEAN_NAME = "messageWeChatTransactionManager";
 
-    @Value("${message.wechat.mysql.connection.url}")
-    private String weChatDbUrl;
-
-    @Value("${message.wechat.mysql.connection.user.name}")
-    private String weChatDbUserName;
-
-    @Value("${message.wechat.mysql.connection.user.password}")
-    private String weChatDbUserPasswordEncrypted;
-
-    @Value("${message.wechat.mysql.connection.max.pool.size}")
-    private Integer weChatDbPoolMaxSize;
-
-    @Value("${message.wechat.mysql.connection.min.pool.size}")
-    private Integer weChatDbPoolMinSize;
-
-    @Bean("messageWeChatDataSource")
-    public DataSource messageWeChatDataSource() {
-
-        HikariDataSource dataSource = DataSourceBuilder.create().type(HikariDataSource.class).build();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setJdbcUrl(weChatDbUrl);
-        dataSource.setUsername(weChatDbUserName);
-
-        String password = new String(Base64.getMimeDecoder().decode(weChatDbUserPasswordEncrypted));
-        dataSource.setPassword(password);
-        //空闲数
-        dataSource.setMinimumIdle(1);
-        dataSource.setMaximumPoolSize(weChatDbPoolMaxSize);
-
-        dataSource.setPoolName("wechat_mysql_thread");
-
-        return dataSource;
-
-    }
-
 
     @Bean(WECHAT_DATA_SOURCE_TRANSACTION_MANAGER_BEAN_NAME)
-    public DataSourceTransactionManager messageWeChatTransactionManager() {
-        return new DataSourceTransactionManager(messageWeChatDataSource());
+    public DataSourceTransactionManager messageWeChatTransactionManager(DataSource companyDataSource) {
+        return new DataSourceTransactionManager(companyDataSource);
     }
 
 
     @Bean(WECHAT_SQL_SESSION_FACTORY_BEAN_NAME)
-    public SqlSessionFactory messageWeChatSqlSessionFactory() {
+    @SuppressWarnings("Duplicates")
+    public SqlSessionFactory messageWeChatSqlSessionFactory(DataSource companyDataSource) {
         TransactionFactory transactionFactory = new SpringManagedTransactionFactory();
-        Environment environment = new Environment("development", transactionFactory, messageWeChatDataSource());
+        Environment environment = new Environment("development", transactionFactory, companyDataSource);
         org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration(environment);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
         return sqlSessionFactory;
     }
 
     @Bean(WECHAT_SQL_SESSION_TEMPLATE_BEAN_NAME)
-    public SqlSessionTemplate messageWeChatSqlSessionTemplate(@Qualifier("messageWeChatSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
-        return new SqlSessionTemplate(sqlSessionFactory, ExecutorType.SIMPLE);
+    public SqlSessionTemplate messageWeChatSqlSessionTemplate(SqlSessionFactory messageWeChatSqlSessionFactory) {
+        return new SqlSessionTemplate(messageWeChatSqlSessionFactory, ExecutorType.SIMPLE);
     }
 
     @Bean
